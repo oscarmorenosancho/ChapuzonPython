da0f1d995dd4307b63eea1aeb222eb12b07fc129
oscarmorenosancho <63343472+oscarmorenosancho@users.noreply.github.com>
2023-03-13 15:25:51 +0100

    Update ex03

--- PM00/ex03/count.py
+++ PM00/ex03/count.py
@@ -6,7 +6,7 @@
 #    By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/13 12:32:27 by omoreno-          #+#    #+#              #
-#    Updated: 2023/03/13 13:23:49 by omoreno-         ###   ########.fr        #
+#    Updated: 2023/03/13 15:21:51 by omoreno-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ def count_set_instr(s :str, set :str) -> int:
 	    acum += s.count(c)
     return(acum)
 
-def text_analyzer(s :str =None):
+def text_analyzer(s =None):
     """text_analyzer es una funcion que toma una string como parametro y 
     cuenta la cantidad de mayusculas, minusculas, signos de puntuacion y 
     espacios.
@@ -26,12 +26,32 @@ def text_analyzer(s :str =None):
     majus = "ABCDEFGHIJKLMNPQRSTUVWXYZ"
     minus = majus.swapcase()
     punt_mark = ",.;:?!"
-    spaces = " "
+    spaces = " \n"
 
     if s == None:
-        print ("Input a string")
+        s = input("The argument is invalid, Introduce a string >")
+    if (not isinstance(s,str)):
+        print ("AssertionError: argument is not a string")
     else:
-       print (count_set_instr(s, majus))
-       print (count_set_instr(s, minus))
-       print (count_set_instr(s, punt_mark))
-       print (count_set_instr(s, spaces))
+        print ("The text contains {} character(s):"
+            .format(len(s)))
+        print ("- {} upper letter(s)"
+            .format(count_set_instr(s, majus)))
+        print ("- {} lower letter(s)"
+            .format(count_set_instr(s, minus)))
+        print ("- {} punctuation mark(s)"
+            .format(count_set_instr(s, punt_mark)))
+        print ("- {} space(s)"
+            .format(count_set_instr(s, spaces)))
+
+if __name__ == "__main__":
+    import sys
+    params = sys.argv[1:]
+    #if len(params) < 1:
+    #    print("AssertionError: no argument is provided")
+    if len(params) > 1:
+        print("AssertionError: more than one argument are provided")
+    elif len(params) < 1:
+        text_analyzer()
+    else:
+        text_analyzer(params[0])
