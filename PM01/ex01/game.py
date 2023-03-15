570f14278da418a08a7c808cdf4a7a3de36aa12a
oscarmorenosancho <63343472+oscarmorenosancho@users.noreply.github.com>
2023-03-15 11:25:10 +0100

    Update ex01 M01

--- PM01/ex01/game.py
+++ PM01/ex01/game.py
@@ -6,7 +6,38 @@
 #    By: omoreno- <omoreno-@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/15 10:55:29 by omoreno-          #+#    #+#              #
-#    Updated: 2023/03/15 10:55:31 by omoreno-         ###   ########.fr        #
+#    Updated: 2023/03/15 11:17:11 by omoreno-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+class GotCharacter:
+    first_name: str
+    is_alive: bool =True
+    def __init__(self, first_name: str, is_alive: bool =True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+class Stark(GotCharacter):
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name = "Stark"
+        self.house_words = "Winter is Coming"
+    
+    def print_house_words(self):
+        print(self.house_words)
+
+    def die(self):
+        self.is_alive = False
+
+class Lannister(GotCharacter):
+    def __init__(self, first_name=None):
+        super().__init__(first_name=first_name)
+        self.family_name = "Lannister"
+        self.house_words = "Winter is never Going"
+        self.weapons = ['sword', 'shield']
+    
+    def print_house_words(self):
+        print(self.house_words)
+
+    def die(self):
+        self.is_alive = False
